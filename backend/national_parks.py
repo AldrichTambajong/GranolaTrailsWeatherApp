b64bc740f6cd2c00f97678d5778bc6d91356fc8b
Michael Anderson <trenonian@gmail.com>
2021-11-13 18:28:30 -0500

    cleaned up functions

--- backend/national_parks.py
+++ backend/national_parks.py
@@ -6,12 +6,14 @@
 # import random
 import os
 import random
-from collections import namedtuple
 import requests
 
 from werkzeug.exceptions import InternalServerError, NotFound
 
+# pylint: disable=import-error
 from open_weather import get_forecast_by_coordinates
+
+# pylint: enable=import-error
 
 # pylint: disable=no-member
 # pylint: disable=wrong-import-position
@@ -57,30 +59,30 @@ def _park_from_data(data):
         _img = data["images"][0]["url"]
     _name = data["fullName"]
     _url = data["url"]
-    _latLong = data["latLong"].split(",")
-    _latitude = _latLong[0].split(":")[1]
-    _longitude = _latLong[1].split(":")[1]
+    _lat_long = data["latLong"].split(",")
+    _latitude = _lat_long[0].split(":")[1]
+    _longitude = _lat_long[1].split(":")[1]
     _weather = get_forecast_by_coordinates(_latitude, _longitude)
 
-    park = {}
-
-    park["id"] = _id
-    park["name"] = _name
-    park["description"] = _description
-    park["url"] = _url
-    park["img"] = _img
-    park["weather"] = _weather
+    park = {
+        "id": _id,
+        "name": _name,
+        "description": _description,
+        "url": _url,
+        "img": _img,
+        "weather": _weather,
+    }
 
     return park
 
 
-def get_parks_by_activities(
-    query: str = None,
+# pylint: disable=too-many-arguments
+def get_parks_and_weather(
     activity_ids: list = None,
-    limit: int = 5,
-    start: int = 0,
-    sort: str = None,
     state: str = "",
+    query: str = None,
+    limit: int = 5,
+    sort: bool = False,
 ) -> list:
     """
     Retrieve national parks that are related to particular categories of activity
@@ -88,19 +90,17 @@ def get_parks_by_activities(
 
     Parameters
     ----------
+    activity_ids: list[str], optional
+        A list of activity IDs.
+    state: str
+        2-character state code
     query: str, optional
         A string to search for.
-    ids: list[str], optional
-        A list of activity IDs.
     limit: int, default 50
         Number of results to return per request.
-    start: int, default 0
-        Get the next [limit] results starting with this number.
-    sort: str, optional
-        A comma delimited list of fields to sort the results by.
-        Ascending order is assumed for each field unless the field
-        name is prefixed with the unary negative which implies
-        descending order.
+    sort: bool, default False
+        True: sorted by name
+        False: random
 
     Returns
     -------
@@ -121,7 +121,7 @@ def get_parks_by_activities(
                 short description of weather
                 https://openweathermap.org/weather-conditions
     """
-    if activity_ids == None:
+    if activity_ids is None:
         activity_ids = []
     try:
         response = requests.get(
@@ -131,24 +131,27 @@ def get_parks_by_activities(
                 "id": ",".join(activity_ids),
                 "q": query,
                 "limit": MAX_LIMIT,
-                "start": start,
-                "sort": sort,
             },
         )
         data = response.json()["data"][0]["parks"]
-        data_state = random.sample(
-            list(filter(lambda p: state in p["states"], data)), limit
-        )
+        data_state = list(filter(lambda p: state in p["states"], data))
+        if sort:
+            data_state = data_state[:limit]
+        else:
+            data_state = random.sample(data_state, limit)
         codes = list(map(lambda p: p["parkCode"], data_state))
         parks = []
-        for i in range(len(codes)):
-            parks.append(get_parks(park_codes=[codes[i]], limit=1)[0])
-            if i > limit:
+        for index, value in enumerate(codes):
+            parks.append(get_parks(park_codes=[value], limit=1)[0])
+            if index > limit:
                 break
         return parks
-    except (InternalServerError, NotFound, TypeError) as e:
-        print(e)
+    except (InternalServerError, NotFound, TypeError) as error:
+        print(error)
         return []
+
+
+# pylint: enable=too-many-arguments
 
 
 # pylint: disable=too-many-arguments
@@ -205,7 +208,7 @@ def get_parks(
             weather: str
     """
 
-    if park_codes == None:
+    if park_codes is None:
         park_codes = []
 
     try:
@@ -215,10 +218,10 @@ def get_parks(
                 "api_key": NATIONAL_PARKS_KEY,
                 "q": query,
                 "parkCode": ",".join(park_codes),
-                # "stateCode": state_code,
-                # "limit": limit,
-                # "start": start,
-                # "sort": sort,
+                "stateCode": state_code,
+                "limit": limit,
+                "start": start,
+                "sort": sort,
             },
         )
         data = response.json()["data"]

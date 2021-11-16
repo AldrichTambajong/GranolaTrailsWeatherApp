b3a76830f784de110c7788f28427136b2a24ef49
Michael Anderson <trenonian@gmail.com>
2021-11-16 15:02:03 -0500

    formatted Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,21 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-    return (
-        <nav className="navbar">
-            <div className="container-fluid">
-                <ul className="header-nav">
-                    <li><Link className="link" to="/home">Home</Link></li>
-                </ul>
+  return (
+    <nav className='navbar'>
+      <div className='container-fluid'>
+        <ul className='header-nav'>
+          <li>
+            <Link className='link' to='/home'>
+              Home
+            </Link>
+          </li>
+        </ul>
 
-                <ul className="header-nav">
-                    <li><Link className="logout" to="/logout">Log Out</Link></li>
-                </ul>
-            </div>
-        </nav>
-
-    )
+        <ul className='header-nav'>
+          <li>
+            <Link className='logout' to='/logout'>
+              Log Out
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;

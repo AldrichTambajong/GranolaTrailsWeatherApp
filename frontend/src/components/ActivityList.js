970599eadb212b6f9be06149311e1a0c9e02ee67
Michael Anderson <trenonian@gmail.com>
2021-11-16 15:08:14 -0500

    open park link in new tab

--- frontend/src/components/ActivityList.js
+++ frontend/src/components/ActivityList.js
@@ -1,66 +1,77 @@
-import { React, useState, useEffect } from 'react'
-import { Card, Button, Container, Row, Col, Spinner } from "react-bootstrap"
+import { React, useState, useEffect } from "react";
+import { Card, Button, Container, Row, Col, Spinner } from "react-bootstrap";
 
 function ActivityList(props) {
+  let activities = {
+    user_state: props.userState,
+  };
 
-    let activities = {
-        'user_state': props.userState
-    }
+  const [listOfActivities, setListOfActivities] = useState([]);
 
-    const [listOfActivities, setListOfActivities] = useState([])
+  useEffect(() => {
+    fetch("/parks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(activities),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setListOfActivities([...data]);
+      });
+    console.log(listOfActivities);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    useEffect(() => {
-        fetch('/parks', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(activities)
-        })
-            .then(response => response.json())
-            .then((data) => {
-                setListOfActivities([...data])
-            })
-        console.log(listOfActivities)
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
-
-    if (listOfActivities.length !== 0) {
-        return (
-            <Container>
-                <Row>
-                    {listOfActivities.map((item, index) => (
-                        <Col>
-                            <Card key={index} style={{ width: '18rem' }}>
-                                <Card.Img variant="top" src={item["img"]} />
-                                <Card.Body>
-                                    <Card.Title>{item["name"]}</Card.Title>
-                                    <Card.Text>
-                                        <p><b>Temperature:</b> {item["weather"]["temperature"]}° F</p>
-                                        <p><b>Precipitation:</b> {item["weather"]["precipitation"]}%</p>
-                                        <p><b>Cloud Coverage:</b> {item["weather"]["clouds"]}%</p>
-                                        <p><b>Condition:</b> {item["weather"]["condition"]}</p>
-                                        {item["description"]}
-                                    </Card.Text>
-                                    <Button variant="primary" onClick={() => {
-                                        window.location.href = item["Park_URL"];
-                                        return null;
-                                    }}>Go To Site</Button>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                    ))}
-                </Row>
-            </Container>
-        )
-    } else {
-        return (
-            <Spinner animation="border" role="status" className="bruv">
-                <span className="visually-hidden">Loading...</span>
-            </Spinner>
-        )
-    }
-
+  if (listOfActivities.length !== 0) {
+    return (
+      <Container>
+        <Row>
+          {listOfActivities.map((item, index) => (
+            <Col>
+              <Card key={index} style={{ width: "18rem" }}>
+                <Card.Img variant='top' src={item["img"]} />
+                <Card.Body>
+                  <Card.Title>{item["name"]}</Card.Title>
+                  <Card.Text>
+                    <p>
+                      <b>Temperature:</b> {item["weather"]["temperature"]}° F
+                    </p>
+                    <p>
+                      <b>Precipitation:</b> {item["weather"]["precipitation"]}%
+                    </p>
+                    <p>
+                      <b>Cloud Coverage:</b> {item["weather"]["clouds"]}%
+                    </p>
+                    <p>
+                      <b>Condition:</b> {item["weather"]["condition"]}
+                    </p>
+                    {item["description"]}
+                  </Card.Text>
+                  <Button
+                    variant='primary'
+                    onClick={() => {
+                      console.log(item);
+                      window.open(item["url"], "_blank");
+                      return null;
+                    }}>
+                    Go To Site
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    );
+  } else {
+    return (
+      <Spinner animation='border' role='status' className='bruv'>
+        <span className='visually-hidden'>Loading...</span>
+      </Spinner>
+    );
+  }
 }
 
-export default ActivityList
+export default ActivityList;

3f9558b0380145b88b0e6b3710ddb4df8694d1f3
Tony Ngo <tony@Tonys-MBP.attlocal.net>
2021-11-30 21:53:23 -0500

    Status for each activity

--- frontend/src/components/ActivityList.js
+++ frontend/src/components/ActivityList.js
@@ -1,5 +1,5 @@
 import { React, useState, useEffect } from "react";
-import { Card, Button, Container, Row, Col, Spinner } from "react-bootstrap";
+import { Card, Button, Container, Row, Col, Spinner, ListGroup, ListGroupItem } from "react-bootstrap";
 
 function ActivityList(props) {
   let activities = {
@@ -30,12 +30,15 @@ function ActivityList(props) {
           {listOfActivities.map((item, index) => (
             <Col>
               <Card key={index} style={{ width: "18rem" }}>
-                <Card.Img variant='top' src={item["img"]} />
+                <Card.Img style={{ width: "100%", height: "200px" }} variant='top' src={item["img"]} />
                 <Card.Body>
                   <Card.Title>{item["name"]}</Card.Title>
                   <Card.Text>
                     <p>
-                      <b>Temperature:</b> {item["weather"]["temperature"]}° F
+                      <b>Temp High:</b> {item["weather"]["high"]}° F
+                    </p>
+                    <p>
+                      <b>Temp Low:</b> {item["weather"]["low"]}° F
                     </p>
                     <p>
                       <b>Precipitation:</b> {item["weather"]["precipitation"]}%
@@ -57,11 +60,18 @@ function ActivityList(props) {
                     Go To Site
                   </Button>
                 </Card.Body>
+                <ListGroup className="list-group-flush">
+                  <ListGroupItem>Bouldering Status: {item["activities"]["bouldering"] ? 'Good to go bouldering!' : 'Do not go bouldering today.'}</ListGroupItem>
+                  <ListGroupItem>Camping Status: {item["activities"]["camping"] ? 'Good to go camping!' : 'Do not go camping today.'}</ListGroupItem>
+                  <ListGroupItem>Fishing Status: {item["activities"]["fishing"] ? 'Good to go fishing!' : 'Do not go fishing today.'}</ListGroupItem>
+                  <ListGroupItem>Hiking Status: {item["activities"]["hiking"] ? 'Good to go hiking!' : 'Do not go hiking today.'}</ListGroupItem>
+                  <ListGroupItem>Off-Roading Status: {item["activities"]["offroading"] ? 'Good to go off-roading!' : 'Do not go offroading today.'}</ListGroupItem>
+                </ListGroup>
               </Card>
             </Col>
           ))}
         </Row>
-      </Container>
+      </Container >
     );
   } else {
     return (

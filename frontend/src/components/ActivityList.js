b1890bc6735cc4916de93c7d3bebc767e9c0049e
Aditya Sharma <adityasharma@adityas-mbp.attlocal.net>
2021-12-03 17:18:15 -0500

    Added icons in Activity page

--- frontend/src/components/ActivityList.js
+++ frontend/src/components/ActivityList.js
@@ -24,7 +24,7 @@ function ActivityList(props) {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(activities),
+      body: JSON.stringify(activities), 
     })
       .then((response) => response.json())
       .then((data) => {
@@ -50,20 +50,20 @@ function ActivityList(props) {
                   <Card.Title>{item["name"]}</Card.Title>
                   <Card.Text>
                     <p>
-                      <i class="fas fa-temperature-high"></i>{item["weather"]["high"]}° F
+                    {item["weather"]["condition"]}
                     </p>
                     <p>
-                      <b>Temp Low:</b> {item["weather"]["low"]}° F
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-thermometer-high" viewBox="0 0 16 16"> <path d="M9.5 12.5a1.5 1.5 0 1 1-2-1.415V2.5a.5.5 0 0 1 1 0v8.585a1.5 1.5 0 0 1 1 1.415z"/> <path d="M5.5 2.5a2.5 2.5 0 0 1 5 0v7.55a3.5 3.5 0 1 1-5 0V2.5zM8 1a1.5 1.5 0 0 0-1.5 1.5v7.987l-.167.15a2.5 2.5 0 1 0 3.333 0l-.166-.15V2.5A1.5 1.5 0 0 0 8 1z"/></svg>{item["weather"]["high"]}° F
                     </p>
                     <p>
-                      <b>Precipitation:</b> {item["weather"]["precipitation"]}%
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-thermometer-low" viewBox="0 0 16 16"><path d="M9.5 12.5a1.5 1.5 0 1 1-2-1.415V9.5a.5.5 0 0 1 1 0v1.585a1.5 1.5 0 0 1 1 1.415z"/><path d="M5.5 2.5a2.5 2.5 0 0 1 5 0v7.55a3.5 3.5 0 1 1-5 0V2.5zM8 1a1.5 1.5 0 0 0-1.5 1.5v7.987l-.167.15a2.5 2.5 0 1 0 3.333 0l-.166-.15V2.5A1.5 1.5 0 0 0 8 1z"/></svg>{item["weather"]["low"]}° F
                     </p>
                     <p>
-                      <b>Cloud Coverage:</b> {item["weather"]["clouds"]}%
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-droplet" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M7.21.8C7.69.295 8 0 8 0c.109.363.234.708.371 1.038.812 1.946 2.073 3.35 3.197 4.6C12.878 7.096 14 8.345 14 10a6 6 0 0 1-12 0C2 6.668 5.58 2.517 7.21.8zm.413 1.021A31.25 31.25 0 0 0 5.794 3.99c-.726.95-1.436 2.008-1.96 3.07C3.304 8.133 3 9.138 3 10a5 5 0 0 0 10 0c0-1.201-.796-2.157-2.181-3.7l-.03-.032C9.75 5.11 8.5 3.72 7.623 1.82z"/><path fill-rule="evenodd" d="M4.553 7.776c.82-1.641 1.717-2.753 2.093-3.13l.708.708c-.29.29-1.128 1.311-1.907 2.87l-.894-.448z"/></svg>  {item["weather"]["precipitation"]}%
                     </p>
                     <p>
-                      <b>Condition:</b> {item["weather"]["condition"]}
-                    </p>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cloud" viewBox="0 0 16 16"><path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383zm.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z"/></svg> {item["weather"]["clouds"]}%
+                    </p>                    
                     {item["description"]}
                   </Card.Text>
                   <Button
